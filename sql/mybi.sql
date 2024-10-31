66779e75239e29908bdfc805ea80d64b78591b36
lzh <1296800094@qq.com>
2024-10-31 10:12:58 +0800

    项目初始化运行

---
+++ sql/mybi.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE IF NOT EXISTS bybi;
+
+USE bybi;
+
+-- 用户表
+create table if not exists user
+(
+    id           bigint auto_increment comment '主键ID' primary key,
+    userAccount
+                 varchar(256)                       not null comment '账号',
+    userPassword varchar(512)                       not null comment '密码',
+    userName     varchar(256)                       null comment '用户昵称',
+    userAvatar   varchar(1024)                      null comment '用户头像',
+    userRole     TINYINT  default 1                 not null comment '用户角色：0-管理员/1-普通用户',
+    createTime   datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint  default 0                 not null comment '是否删除',
+    UNIQUE idx_userAccount (userAccount)
+) comment '用户' collate = utf8mb4_unicode_ci;
+
+
+-- 图表信息表
+create table if not exists chart
+(
+    id         bigint auto_increment comment '主键ID' primary key,
+    goal       text                               null comment '分析目标',
+    chartData  text                               null comment '图表数据',
+    chartType  varchar(128)                       null comment '图表类型',
+    genChart   text                               null comment '生成的图表数据',
+    genResult  text                               null comment '生成的分析结论',
+    userId     bigint                             null comment '创建用户 ID',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除'
+) comment '图表信息表' collate = utf8mb4_unicode_ci;
---
+++ src/main/java/com/lzh/bi/BetterBiBackendApplication.java
@@ -0,0 +1,16 @@
+package com.lzh.bi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lzh
+ */
+@SpringBootApplication
+public class BetterBiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BetterBiBackendApplication.class, args);
+    }
+
+}
